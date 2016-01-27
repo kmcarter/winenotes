cb0601cf6de54845225d739efc7e0a93ec382989
Kelly Carter <kelly@rainworksweb.com>
2016-01-27 04:52:40 +0000

    Added wine concept, spec passes

--- spec/features/list_notes_spec.rb
+++ spec/features/list_notes_spec.rb
@@ -8,3 +8,12 @@ RSpec.feature 'Listing all notes' do
         expect(page).to have_content('0 notes')
     end
 end
+
+RSpec.feature 'Listing all wines' do
+    scenario 'returns a message when there are no wines to view' do
+        visit '/'
+        click_link 'Wines'
+        expect(page.current_url).to eq(wines_url)
+        expect(page).to have_content('0 wines')
+    end
+end
